7f1ef3889646acefb9fc15d73fb9bde0c9265956
jose manuel <josemmanuelwar@hotmail.es>
2022-08-28 19:40:17 -0500

    agragando parametros a las ruta

--- Controllers/AutoresController.cs
+++ Controllers/AutoresController.cs
@@ -22,10 +22,41 @@ namespace WebAPIAutores.Controllers
         }
 
         [HttpGet]
+        [HttpGet("listado")]// api/autores/listado
+        [HttpGet("/listado")]// listado
         public async Task<ActionResult<List<Autor>>> Get()
         {
             var lista_autores = await this.context.Autores.Include(x => x.Libros).ToListAsync();
             return lista_autores; 
+        }
+
+        [HttpGet("primero")]
+        public async Task<ActionResult<Autor>> Primero(){
+            return await this.context.Autores.FirstOrDefaultAsync();
+        }
+
+        [HttpGet("{id:int}/{param2?}")]// pasamos dos parametros pero el segundo es opcional
+        //parametro definido de esta forma [HttpGet("{id:int}/{param2=persona}")]
+        public async Task<ActionResult<Autor>> Get(int id, string param2 )
+        {
+            var autor = await context.Autores.FirstOrDefaultAsync(x => x.Id == id);
+            
+            if(autor == null){
+                return NotFound();
+            }
+
+            return autor;
+        }
+
+        [HttpGet("{nombre}")]
+        public async Task<ActionResult<Autor>> Get(string nombre){
+            var autor = await this.context.Autores.FirstOrDefaultAsync(
+                x => x.Nombre.Contains(nombre));
+            
+            if(autor == null){
+                return NotFound();
+            }
+            return autor;
         }
 
         [HttpPost]
@@ -37,7 +68,7 @@ namespace WebAPIAutores.Controllers
         }
 
         [HttpPut("{id:int}")]
-        public async Task<ActionResult>Put(Autor autor,int id){
+        public async Task<ActionResult> Put(Autor autor,int id){
             if(autor.Id != id){
                 return BadRequest("El id del autor no concide con el id de la url");
             }
@@ -53,7 +84,7 @@ namespace WebAPIAutores.Controllers
         }
 
         [HttpDelete("{id:int}")]
-        public async Task<ActionResult>Delete(int id){
+        public async Task<ActionResult> Delete(int id){
             var existe = await this.context.Autores.AnyAsync(x => x.Id == id);
             if(!existe){
                 return NotFound();
